e0afdc21777eb538cd18e30465fe4b2ce66af75d
alonazrnko <alonazrnko@gmail.com>
2026-02-10 15:52:59 +0300

    feat: implement User model, DTO and mapper

---
+++ src/main/java/ru/practicum/user/UserMapper.java
@@ -0,0 +1,23 @@
+package ru.practicum.user;
+
+import ru.practicum.user.dto.UserDto;
+import ru.practicum.user.model.User;
+
+public class UserMapper {
+
+    public static UserDto toUserDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+
+    public static User toUser(UserDto userDto) {
+        return User.builder()
+                .id(userDto.getId())
+                .name(userDto.getName())
+                .email(userDto.getEmail())
+                .build();
+    }
+}
