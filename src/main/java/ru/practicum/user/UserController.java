3d223ebaf07e84ea5c941115f2c61f5002816310
alonazrnko <alonazrnko@gmail.com>
2026-02-10 15:55:03 +0300

    feat: implement UserController with logging

--- src/main/java/ru/practicum/user/UserController.java
+++ src/main/java/ru/practicum/user/UserController.java
@@ -1,23 +1,47 @@
 package ru.practicum.user;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.user.dto.UserDto;
 
 import java.util.List;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/users")
+@RequiredArgsConstructor
+@Slf4j
 public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<User> getAllUsers() {
+    public List<UserDto> findAll() {
+        log.info("Received request to find all users");
         return userService.getAllUsers();
     }
 
+    @GetMapping("/{id}")
+    public UserDto findById(@PathVariable Long id) {
+        log.info("Received request to find user with id: {}", id);
+        return userService.getUserById(id);
+    }
+
     @PostMapping
-    public User saveNewUser(@RequestBody User user) {
-        return userService.saveUser(user);
+    public UserDto create(@Valid @RequestBody UserDto userDto) {
+        log.info("Received request to create user: {}", userDto);
+        return userService.createUser(userDto);
+    }
+
+    @PatchMapping("/{id}")
+    public UserDto update(@PathVariable Long id, @RequestBody UserDto userDto) {
+        log.info("Received request to update user with id: {}", id);
+        return userService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        log.info("Received request to delete user with id: {}", id);
+        userService.deleteUser(id);
     }
 }
