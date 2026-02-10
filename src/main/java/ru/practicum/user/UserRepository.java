52009f1247e5cc70ee994ec6665fd13480a8c66d
alonazrnko <alonazrnko@gmail.com>
2026-02-10 15:54:13 +0300

    feat: implement in-memory UserRepository and UserService

--- src/main/java/ru/practicum/user/UserRepository.java
+++ src/main/java/ru/practicum/user/UserRepository.java
@@ -1,9 +1,19 @@
 package ru.practicum.user;
 
+import ru.practicum.user.model.User;
+
 import java.util.List;
+import java.util.Optional;
 
 interface UserRepository {
     List<User> findAll();
+
+    Optional<User> findById(Long id);
+
     User save(User user);
+
+    void delete(Long id);
+
+    boolean existsByEmail(String email);
 }
 
--- src/main/java/ru/practicum/user/UserService.java
+++ src/main/java/ru/practicum/user/UserService.java
@@ -1,8 +1,18 @@
 package ru.practicum.user;
 
+import ru.practicum.user.dto.UserDto;
+import ru.practicum.user.model.User;
+
 import java.util.List;
 
 interface UserService {
-    List<User> getAllUsers();
-    User saveUser(User user);
+    List<UserDto> getAllUsers();
+
+    UserDto getUserById(Long id);
+
+    UserDto createUser(UserDto userDto);
+
+    UserDto updateUser(Long id, UserDto userDto);
+
+    void deleteUser(Long id);
 }
