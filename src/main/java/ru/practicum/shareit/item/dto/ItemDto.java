e1bcad7e1b2dbe8fabb7667ed74a2e438965d1b2
alonazrnko <alonazrnko@gmail.com>
2026-03-03 18:35:24 +0300

    feat: add comments for items and update item service logic

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,39 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ItemDto {
+    private Long id;
+
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotBlank(message = "Description cannot be empty")
+    private String description;
+
+    @NotNull(message = "Availability status is required")
+    private Boolean available;
+
+    private Long requestId;
+
+    private BookingShortDto lastBooking;
+    private BookingShortDto nextBooking;
+
+    private List<CommentDto> comments;
+
+    @Data
+    @AllArgsConstructor
+    public static class BookingShortDto {
+        private Long id;
+        private Long bookerId;
+    }
 }
