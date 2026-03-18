c9486e3e91c9630e0a019f4153dcadc92d1b1bef
alonazrnko <alonazrnko@gmail.com>
2026-03-18 13:12:47 +0300

    feat: extract Booking validation to API Gateway and configure REST client

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingPostDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingPostDto.java
@@ -1,5 +1,8 @@
 package ru.practicum.shareit.booking.dto;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -7,6 +10,10 @@ import java.time.LocalDateTime;
 @Data
 public class BookingPostDto {
     private Long itemId;
+    @NotNull
+    @FutureOrPresent
     private LocalDateTime start;
+    @NotNull
+    @Future
     private LocalDateTime end;
 }
