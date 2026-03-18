135afed826b4983584ce8b5d31ba04ec78f2d458
alonazrnko <alonazrnko@gmail.com>
2026-03-18 12:10:41 +0300

    build: configure maven multi-module setup (gateway and server)

---
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingClient.java
@@ -0,0 +1,6 @@
+package ru.practicum.shareit.booking;
+
+import ru.practicum.shareit.client.BaseClient;
+
+public class BookingClient extends BaseClient {
+}
---
+++ gateway/src/main/java/ru/practicum/shareit/client/BaseClient.java
@@ -0,0 +1,4 @@
+package ru.practicum.shareit.client;
+
+public class BaseClient {
+}
