6ad674b93b08a982be7336d3739beb978fae8a79
alonazrnko <alonazrnko@gmail.com>
2026-03-19 12:25:30 +0300

    test: add mvc and unit tests for user module in gateway and server

---
+++ gateway/src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -0,0 +1,60 @@
+package ru.practicum.shareit.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.user.dto.UserDto;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserClient userClient;
+
+    @Test
+    void createUser_WhenValid_ShouldReturnOk() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setName("Test Name");
+        userDto.setEmail("test@mail.com");
+
+        when(userClient.createUser(any(UserDto.class)))
+                .thenReturn(ResponseEntity.ok().build());
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userDto)))
+                .andExpect(status().isOk());
+
+        verify(userClient, times(1)).createUser(any(UserDto.class));
+    }
+
+    @Test
+    void createUser_WhenEmailInvalid_ShouldReturnBadRequest() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setName("Test Name");
+        userDto.setEmail("invalid-email-format");
+
+        mockMvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userDto)))
+                .andExpect(status().isBadRequest());
+
+        verify(userClient, never()).createUser(any(UserDto.class));
+    }
+}
---
+++ server/src/test/java/ru/practicum/shareit/user/UserServiceImplTest.java
@@ -0,0 +1,82 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.dto.UserMapper;
+import ru.practicum.shareit.user.model.User;
+import ru.practicum.shareit.user.repository.UserRepository;
+import ru.practicum.shareit.user.service.UserServiceImpl;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceImplTest {
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    private User user;
+    private UserDto userDto;
+
+    @BeforeEach
+    void setUp() {
+        user = new User();
+        user.setId(1L);
+        user.setName("Test Name");
+        user.setEmail("test@mail.com");
+
+        // Initialize expected DTO
+        userDto = new UserDto();
+        userDto.setId(1L);
+        userDto.setName("Test Name");
+        userDto.setEmail("test@mail.com");
+    }
+
+    @Test
+    void getUserById_WhenUserExists_ShouldReturnUserDto() {
+        when(userRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        when(userMapper.toUserDto(user)).thenReturn(userDto);
+
+        UserDto result = userService.getById(1L);
+
+        assertNotNull(result);
+        assertEquals(userDto.getId(), result.getId());
+        assertEquals(userDto.getName(), result.getName());
+
+        verify(userMapper, times(1)).toUserDto(user);
+    }
+
+    @Test
+    void getUserById_WhenUserDoesNotExist_ShouldThrowNotFoundException() {
+        when(userRepository.findById(anyLong())).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> userService.getById(999L));
+
+        verify(userMapper, never()).toUserDto(any());
+    }
+
+    @Test
+    void deleteUser_ShouldInvokeRepositoryDelete() {
+        userService.delete(1L);
+
+        verify(userRepository, times(1)).deleteById(1L);
+    }
+}
